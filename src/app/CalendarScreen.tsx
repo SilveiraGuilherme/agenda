9001e7e91f16fcac965ac5ec9dd0a38062ee2c4d
Guilherme Silveira <108364424+SilveiraGuilherme@users.noreply.github.com>
2023-02-01 16:15:38 -0300

    Added events

--- src/app/CalendarScreen.tsx
+++ src/app/CalendarScreen.tsx
@@ -10,10 +10,21 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import Icon from '@material-ui/core/Icon';
+import { getEventsEndpoint, IEvent } from './backend';
+import { useEffect, useState } from 'react';
 
-const DAYS_OF_THE_WEEK = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+const DAYS_OF_THE_WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
 export default function CalendarScreen() {
+  const [events, setEvents] = useState<IEvent[]>([]);
+  const weeks = generateCalendar(getToday(), events);
+  const firstDay = weeks[0][0].date;
+  const lastDay = weeks[weeks.length - 1][6].date;
+
+  useEffect(() => {
+    getEventsEndpoint(firstDay, lastDay).then(setEvents);
+  }, [firstDay, lastDay]);
+
   return (
     <Box display="flex" height="100%" alignItems="stretch">
       <Box component={'section'} width="12em" padding="8px 16px">
@@ -59,6 +70,7 @@ export default function CalendarScreen() {
             borderRight: 1,
             borderTop: 1,
             borderColor: 'grey.300',
+            tableLayout: 'fixed',
           }}
           aria-label="simple table"
         >
@@ -81,57 +93,111 @@ export default function CalendarScreen() {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              {DAYS_OF_THE_WEEK.map(day => {
-                return (
-                  <TableCell
-                    sx={{
-                      borderLeft: 1,
-                      borderColor: 'grey.300',
-                    }}
-                    align="center"
-                    key={day}
-                  >
-                    x
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-            <TableRow>
-              {DAYS_OF_THE_WEEK.map(day => {
-                return (
-                  <TableCell
-                    sx={{
-                      borderLeft: 1,
-                      borderColor: 'grey.300',
-                    }}
-                    align="center"
-                    key={day}
-                  >
-                    y
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-            <TableRow>
-              {DAYS_OF_THE_WEEK.map(day => {
-                return (
-                  <TableCell
-                    sx={{
-                      borderLeft: 1,
-                      borderColor: 'grey.300',
-                    }}
-                    align="center"
-                    key={day}
-                  >
-                    z
-                  </TableCell>
-                );
-              })}
-            </TableRow>
+            {weeks.map((week, i) => {
+              return (
+                <TableRow key={i}>
+                  {week.map(cell => {
+                    return (
+                      <TableCell
+                        sx={{
+                          borderLeft: 1,
+                          borderColor: 'grey.300',
+                          verticalAlign: 'top',
+                          overflow: 'hidden',
+                          padding: '8px 4px',
+                        }}
+                        align="center"
+                        key={cell.date}
+                      >
+                        <Box
+                          component={'div'}
+                          sx={{
+                            fontWeight: 500,
+                            marginBottom: '4px',
+                          }}
+                        >
+                          {cell.dayOfMonth}
+                        </Box>
+                        {cell.events.map(event => {
+                          return (
+                            <Box
+                              component={'button'}
+                              key={event.id}
+                              sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'left',
+                                background: 'none',
+                                border: 'none',
+                                cursor: 'pointer',
+                                whiteSpace: 'nowrap',
+                                margin: '4px 0',
+                              }}
+                            >
+                              {event.time && (
+                                <Icon fontSize="inherit">watch_later</Icon>
+                              )}
+                              {event.time && (
+                                <Box component={'span'} margin="0 4px">
+                                  {event.time}
+                                </Box>
+                              )}
+                              <span>{event.desc}</span>
+                            </Box>
+                          );
+                        })}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
     </Box>
   );
+}
+
+interface ICalendarCell {
+  date: string;
+  dayOfMonth: number;
+  events: IEvent[];
+}
+
+function generateCalendar(
+  date: string,
+  allEvents: IEvent[]
+): ICalendarCell[][] {
+  const weeks: ICalendarCell[][] = [];
+  const jsDate = new Date(date + 'T12:00:00');
+  const currentMonth = jsDate.getMonth();
+
+  const currentDay = new Date(jsDate.valueOf());
+  currentDay.setDate(1);
+  const dayOfWeek = currentDay.getDay();
+  currentDay.setDate(1 - dayOfWeek);
+
+  do {
+    const week: ICalendarCell[] = [];
+    for (let i = 0; i < DAYS_OF_THE_WEEK.length; i++) {
+      const monthStr = (currentDay.getMonth() + 1).toString().padStart(2, '0');
+      const dayStr = currentDay.getDate().toString().padStart(2, '0');
+      const isoDate = `${currentDay.getFullYear()}-${monthStr}-${dayStr}`;
+
+      week.push({
+        dayOfMonth: currentDay.getDate(),
+        date: isoDate,
+        events: allEvents.filter(e => e.date === isoDate),
+      });
+      currentDay.setDate(currentDay.getDate() + 1);
+    }
+    weeks.push(week);
+  } while (currentDay.getMonth() === currentMonth);
+
+  return weeks;
+}
+
+function getToday() {
+  return '2021-06-03';
 }
