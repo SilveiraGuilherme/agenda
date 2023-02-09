4be9c553de7b9ab626c52aba17f8e3bd8a50ff2a
Guilherme Silveira <108364424+SilveiraGuilherme@users.noreply.github.com>
2023-02-09 09:46:51 -0300

    Fixed type error on CreateEventEndpoint

--- src/app/backend.tsx
+++ src/app/backend.tsx
@@ -34,7 +34,7 @@ export function createEventEndpoint(event: IEditingEvent): Promise<IEvent> {
   return fetch(`http://localhost:8080/events`, {
     method: 'POST',
     headers: {
-      'Content.Type': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(event),
   }).then(resp => {
